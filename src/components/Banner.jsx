d5859c03fcc25646c8c5fa64ac6827707e7b691b
Adam Abdulkadir <adama1862@outlook.com>
2026-02-09 01:59:29 -0500

    favicon

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,13 +4,7 @@ import BannerImage from "../assets/images/banner.png";
 export default function Banner() {
   return (
     <div className="banner">
-      <img src="src/assets/images/banner.png" alt="Adams Verse Banner" />
-      <img
-        src={BannerImage}
-        srcSet={`${BannerImage} 1x, ${HighResBannerImage} 2x`}
-        alt="Banner"
-        loading="lazy"
-      />
+      <img src={BannerImage} alt="Adams Verse Banner" />
     </div>
   );
 }
