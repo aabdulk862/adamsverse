15ff9eca3315c5309a6f0991c0b3a2b61f897092
Adam Abdulkadir <78934570+aabdulk862@users.noreply.github.com>
2026-02-11 13:35:02 -0500

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,8 +3,10 @@ import BannerImage from "../assets/images/banner.png";
 
 export default function Banner() {
   return (
+  <div className="container">
     <div className="banner">
       <img src={BannerImage} alt="Adams Verse Banner" />
     </div>
-  );
+  </div>
+);
 }
