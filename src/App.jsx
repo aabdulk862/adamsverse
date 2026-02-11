072ae82447eb56cafc85512169a3c856d3a07865
Adam Abdulkadir <78934570+aabdulk862@users.noreply.github.com>
2026-02-11 11:02:09 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,38 @@ import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import Services from "./pages/Services";
 import Projects from "./pages/Projects";
+
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-     <Banner />
+      {/* Banner sits above Navbar */}
+      <Banner />
+
+      {/* Navbar */}
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:slug" element={<BlogPost />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+
+      {/* Main content container */}
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+
+      {/* Footer */}
+      <Footer />
     </BrowserRouter>
   );
 }
